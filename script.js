4d117714bb48c52a0c6b40c539ae27ad979d8e78
Julia Yakutova <julia.yakutova@gmail.com>
2021-04-09 19:16:39 +0300

    Simple converter realisation.

---
+++ script.js
@@ -0,0 +1,29 @@
+const emojiHash = {
+  0: '&#128347;',
+  1: '&#128336;',
+  2: '&#128337;',
+  3: '&#128338;',
+  4: '&#128339;',
+  5: '&#128340;',
+  6: '&#128341;',
+  7: '&#128342;',
+  8: '&#128343;',
+  9: '&#128344;'
+};
+
+function convert() {
+  const str = document.getElementById('numbers').value;
+  let convertedStr = '';
+
+  str.trim()
+    .split('').forEach(char => {
+      if(isNaN(char) || char === ' '){
+        convertedStr += char;
+      } else {
+        const number = parseInt(char);
+        convertedStr += emojiHash[number];
+      }
+  });
+
+  document.getElementById('result').innerHTML = convertedStr;
+}
